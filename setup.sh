6da742e074e9ed508d393f7fb131f09e778956e7
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-12-17 10:51:10 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+{
+    touch command.sh
+    echo '#bin/bash' > command.sh
+    echo 'echo -en ''Enter Your short command letter (example :- c): '>>command.sh
+    echo 'read Let'>>command.sh
+    echo 'echo -en ''Enter command (example :- clear) : '>>command.sh
+    echo 'read Scr'>>command.sh
+    echo 'cd'>>command.sh
+    echo 'cd ../usr/bin'>>command.sh
+    echo '{'>>command.sh
+    echo 'touch $Let'>>command.sh
+    echo 'echo ''$Scr''>>$Let'>>command.sh
+    echo '}'>>command.sh
+    echo 'chmod 777 $Let'>>command.sh
+}
